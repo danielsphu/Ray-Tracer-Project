de9837df885f763bd9339b753934545be6dfa73d
Daniel Hu <65648964+danielsphu@users.noreply.github.com>
2023-07-23 11:36:48 -0400

    added a positional camera

--- Ray Tracer Project/main.cpp
+++ Ray Tracer Project/main.cpp
@@ -37,13 +37,9 @@ int main() {
     const int samples_per_pixel = 100;
     const int max_depth = 50;
 
-    // Camera
-
-    camera cam;
-
-    // World
-
+    //World
     hittable_list world;
+
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
     auto material_left   = make_shared<dielectric>(1.5);
@@ -52,7 +48,11 @@ int main() {
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
     world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
+    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0), -0.45, material_left));
     world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
+
+    //Camera
+    camera cam(90, aspect_ratio, point3(-2,2,1), point3(0,0,-1), vec3(0,1,0));
 
     // Render
 
