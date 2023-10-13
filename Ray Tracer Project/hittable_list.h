764ca0614a21eeb450e3d2e5e2a46c4f5c6f4682
Daniel Hu <65648964+danielsphu@users.noreply.github.com>
2023-10-13 16:39:37 -0400

    updated BVH

--- Ray Tracer Project/hittable_list.h
+++ Ray Tracer Project/hittable_list.h
@@ -1,62 +1,50 @@
 #ifndef HITTABLE_LIST_H
 #define HITTABLE_LIST_H
 
+#include "rtweekend.h"
+
+#include "aabb.h"
 #include "hittable.h"
-#include "axis-aligned-bounding-box.h"
 
 #include <memory>
 #include <vector>
 
-using std::shared_ptr;
-using std::make_shared;
 
 class hittable_list : public hittable {
-    public:
-        hittable_list() {}
-        hittable_list(shared_ptr<hittable> object) { add(object); }
+  public:
+    std::vector<shared_ptr<hittable>> objects;
 
-        void clear() { objects.clear(); }
-        void add(shared_ptr<hittable> object) { objects.push_back(object); }
+    hittable_list() {}
+    hittable_list(shared_ptr<hittable> object) { add(object); }
 
-        virtual bool hit(
-            const ray& r, double t_min, double t_max, hit_record& rec) const override;
+    void clear() { objects.clear(); }
 
-        virtual bool bounding_box(
-            double time0, double time1, aabb& output_box) const override;
+    void add(shared_ptr<hittable> object) {
+        objects.push_back(object);
+        bbox = aabb(bbox, object->bounding_box());
+    }
 
-    public:
-        std::vector<shared_ptr<hittable>> objects;
+    bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
+        hit_record temp_rec;
+        auto hit_anything = false;
+        auto closest_so_far = ray_t.max;
+
+        for (const auto& object : objects) {
+            if (object->hit(r, interval(ray_t.min, closest_so_far), temp_rec)) {
+                hit_anything = true;
+                closest_so_far = temp_rec.t;
+                rec = temp_rec;
+            }
+        }
+
+        return hit_anything;
+    }
+
+    aabb bounding_box() const override { return bbox; }
+
+  private:
+    aabb bbox;
 };
 
-bool hittable_list::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
-    hit_record temp_rec;
-    bool hit_anything = false;
-    auto closest_so_far = t_max;
-
-    for (const auto& object : objects) {
-        if (object->hit(r, t_min, closest_so_far, temp_rec)) {
-            hit_anything = true;
-            closest_so_far = temp_rec.t;
-            rec = temp_rec;
-        }
-    }
-
-    return hit_anything;
-}
-
-bool hittable_list::bounding_box(double time0, double time1, aabb& output_box) const {
-    if (objects.empty()) return false;
-
-    aabb temp_box;
-    bool first_box = true;
-
-    for (const auto& object : objects) {
-        if (!object->bounding_box(time0, time1, temp_box)) return false;
-        output_box = first_box ? temp_box : surrounding_box(output_box, temp_box);
-        first_box = false;
-    }
-
-    return true;
-}
 
 #endif
--- Ray Tracer Project/ray.h
+++ Ray Tracer Project/ray.h
@@ -4,22 +4,28 @@
 #include "vec3.h"
 
 class ray {
-    public:
-        ray() {}
-        ray(const point3& origin, const vec3& direction)
-            : orig(origin), dir(direction)
-        {}
+  public:
+    ray() {}
 
-        point3 origin() const  { return orig; }
-        vec3 direction() const { return dir; }
+    ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction), tm(0)
+    {}
 
-        point3 at(double t) const {
-            return orig + t*dir;
-        }
+    ray(const point3& origin, const vec3& direction, double time)
+      : orig(origin), dir(direction), tm(time)
+    {}
 
-    public:
-        point3 orig;
-        vec3 dir;
+    point3 origin() const  { return orig; }
+    vec3 direction() const { return dir; }
+    double time() const    { return tm; }
+
+    point3 at(double t) const {
+        return orig + t*dir;
+    }
+
+  private:
+    point3 orig;
+    vec3 dir;
+    double tm;
 };
 
 #endif
